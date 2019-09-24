f5d7976a10e4ccd9f159d7e059229d76552b25f9
Kyle Rubenok <kyle@rubenok.ca>
2019-09-24 14:06:36 -0400

    Add .env file and deploy mode

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const VoiceResponse = require("twilio").twiml.VoiceResponse;
-const urlencoded = require("body-parser").urlencoded;
+express = require("express");
+VoiceResponse = require("twilio").twiml.VoiceResponse;
+urlencoded = require("body-parser").urlencoded;
+dotenv = require('dotenv').config();
 
-const app = express();
+app = express();
 
 // Parse incoming POST params with Express middleware
 app.use(urlencoded({ extended: false }));
@@ -11,11 +12,11 @@ app.use(urlencoded({ extended: false }));
 // HTTP POST to /voice in our application
 app.post("/voice", (request, response) => {
     // Use the Twilio Node.js SDK to build an XML response
-    const twiml = new VoiceResponse();
+    twiml = new VoiceResponse();
 
     /** helper function to set up a <Gather> */
     function gather() {
-        const gatherNode = twiml.gather({ numDigits: 4 });
+        gatherNode = twiml.gather({ numDigits: 4 });
         gatherNode.say("Please enter your access code:");
 
         // If the user doesn't enter input, loop
@@ -25,15 +26,15 @@ app.post("/voice", (request, response) => {
     // If the user entered digits, process their request
     if (request.body.Digits) {
         switch (request.body.Digits) {
-            case "1234":
-                twiml.say("You entered 1 2 3 4");
+            case process.env.ACCESS_CODE:
+                twiml.say("Code accepted! Welcome to La Terrasse.");
                 twiml.play({
                     digits:'w999'
                 });
                 console.log(request.body.Digits);
                 break;
             default:
-                twiml.say("Sorry, I don't understand that choice.").pause();
+                twiml.say("Sorry, that code was invalid, try again.").pause();
                 gather();
                 break;
         }
@@ -47,6 +48,15 @@ app.post("/voice", (request, response) => {
     response.send(twiml.toString());
 });
 
-// Create an HTTP server and listen for requests on port 3000
-console.log("Twilio Client app HTTP server running at http://127.0.0.1:1337");
-app.listen(1337);
+// Create an HTTP server and listen for requests
+if (process.env.DEPLOY_MODE == "DEV") {
+    app.listen(1337);
+    console.log("Twilio Client app HTTP server running at http://127.0.0.1:1337");
+}
+else if (process.env.DEPLOY_MODE == "PROD") {
+    app.listen(443);
+    console.log("Twilio Client app HTTP server running at http://127.0.0.1:443");
+}
+else {
+    console.log("Error in .env file. Check DEPLOY_MODE.");
+}
