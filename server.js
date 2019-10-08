a3df9c42058e46aaf47e06cd735a0f2f90bf1a1d
Kyle Rubenok <kyle@rubenok.ca>
2019-10-07 23:24:28 -0400

    welp that's a lot simpler than I thought

--- server.js
+++ server.js
@@ -13,38 +13,14 @@ app.use(urlencoded({ extended: false }));
 app.post("/voice", (request, response) => {
     // Use the Twilio Node.js SDK to build an XML response
     twiml = new VoiceResponse();
-
-    /** helper function to set up a <Gather> */
-    function gather() {
-        gatherNode = twiml.gather({ numDigits: 4 });
-        gatherNode.say(precess.end.GREETING);
-
-        // If the user doesn't enter input, loop
-        twiml.redirect("/voice");
-    }
-
-    // If the user entered digits, process their request
-    if (request.body.Digits) {
-        switch (request.body.Digits) {
-            case process.env.ACCESS_CODE:
-                twiml.say(precess.end.ACCEPTED);
-                twiml.play({
-                    // Plays DFTM tones passed in .env file. 
-                    digits: process.env.TONE
-                });
-                console.log(request.body.Digits);
-                break;
-            default:
-                twiml.say(precess.end.REJECTED).pause();
-                gather();
-                break;
-        }
-    } else {
-        // If no input was sent, use the <Gather> verb to collect user input
-        gather();
-    }
-
-    // Render the response as XML in reply to the webhook request
+    twiml.play({
+        digits: "w999"
+    });
+    const dial = twiml.dial();
+    dial.number({
+        sendDigits: 'w999'
+    });
+    twiml.say("Hello");
     response.type("text/xml");
     response.send(twiml.toString());
 });
